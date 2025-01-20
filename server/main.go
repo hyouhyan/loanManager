4a143c7e310cd0b2e10f71d55c0e9f3f222fb6ca
Hyouhyan <hyouhyan@hyouhyan.com>
2025-01-20 18:12:18 +0900

    feat: ユーザーを追加できるようにした

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -16,7 +17,7 @@ type ShowUserHandler struct{}
 func (h *ShowUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	DbConnection, err := sql.Open("sqlite3", "./loanManager.db")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	cmd := "SELECT * FROM user"
@@ -28,7 +29,7 @@ func (h *ShowUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		var p User
 		err := rows.Scan(&p.id, &p.name, &p.email, &p.password)
 		if err != nil {
-			log.Fatalln(err)
+			log.Panicln(err)
 		}
 		pp = append(pp, p)
 	}
@@ -42,7 +43,7 @@ type ShowCoUserHandler struct{}
 func (h *ShowCoUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	DbConnection, err := sql.Open("sqlite3", "./loanManager.db")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	cmd := "SELECT * FROM coUser"
@@ -54,7 +55,7 @@ func (h *ShowCoUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		var p CoUser
 		err := rows.Scan(&p.id, &p.name, &p.parentId)
 		if err != nil {
-			log.Fatalln(err)
+			log.Panicln(err)
 		}
 		pp = append(pp, p)
 	}
@@ -65,6 +66,48 @@ func (h *ShowCoUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (h *HogeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hoge")
+}
+
+type RequestBody struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserAddHandler struct{}
+
+func (h *UserAddHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "POSTメソッドのみ受け付けています", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// リクエストボディの読み取り
+	var requestBody RequestBody
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		http.Error(w, "リクエストの解析に失敗しました", http.StatusBadRequest)
+		return
+	}
+
+	// データ検証
+	if requestBody.Name == "" || requestBody.Email == "" || requestBody.Password == "" {
+		http.Error(w, "なんか足りません", http.StatusBadRequest)
+		return
+	}
+
+	DbConnection, err := sql.Open("sqlite3", "./loanManager.db")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	cmd := "INSERT INTO user (name, email, password) VALUES (?, ?, ?)"
+	_, err = DbConnection.Exec(cmd, requestBody.Name, requestBody.Email, requestBody.Password)
+	if err != nil {
+		http.Error(w, "なんかエラーです", http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
 }
 
 type User struct {
@@ -92,6 +135,7 @@ func main() {
 	hoge := HogeHandler{}
 	showuser := ShowUserHandler{}
 	showcouser := ShowCoUserHandler{}
+	useradd := UserAddHandler{}
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -102,6 +146,8 @@ func main() {
 	http.Handle("/show/user", &showuser)
 	http.Handle("/show/co-user", &showcouser)
 	http.Handle("/hoge", &hoge)
+
+	http.Handle("/user/add", &useradd)
 
 	fmt.Println("Listening on http://localhost:8080")
 	server.ListenAndServe()
