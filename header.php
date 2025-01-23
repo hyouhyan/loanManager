ac974fcb30b6c1e6fd71198fe7334d1efcce4367
Hyouhyan <hyouhyan@hyouhyan.com>
2025-01-23 19:14:06 +0900

    モバイル用のハンバーガーメニュー実装

--- header.php
+++ header.php
@@ -12,11 +12,14 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="index.php">Loan Manager</a>
-            <div class="collapse navbar-collapse">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <?php if (isset($_SESSION['user_id'])): ?>
                         <li class="nav-item"><a class="nav-link" href="add_contact.php">取引先追加</a></li>
-                        <li class="nav-item"><a class="nav-link" href="edit_userinfo.php">情報編集</a></li>
+                        <li class="nav-item"><a class="nav-link" href="edit_userinfo.php">ユーザー編集</a></li>
                         <li class="nav-item"><a class="nav-link" href="logout.php">ログアウト</a></li>
                     <?php else: ?>
                         <li class="nav-item"><a class="nav-link" href="login.php">ログイン</a></li>
@@ -27,3 +30,4 @@
         </div>
     </nav>
     <div class="container mt-5">
+        <!-- メインコンテンツ -->
