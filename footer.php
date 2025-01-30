0bbbf69961fa86365fff635c7a8d5f79cc22b604
Hyouhyan <hyouhyan@hyouhyan.com>
2025-01-30 21:09:56 +0900

    feat: サービスワーカー追加

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        // ServiceWorker登録：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('sw.js').then(function(registration) {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(function(err) {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        }
+    </script>
 </body>
 </html>
